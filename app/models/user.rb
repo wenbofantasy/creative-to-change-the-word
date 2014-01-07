f073bacd30bfa2548185ce913f31631ea3f9f03c
wenbofantasy <wenbofantasy@gmail.com>
2014-01-07 19:55:26 +0800

    1.7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
 
 
-  attr_accessible :accounts, :certificates, :choose_file, :contact_address, :country, :date_birth, :email, :excepetation_industry, :excepetation_job, :experience_year, :job_status, :landline, :language_ability, :mobile, :name, :number, :pinyin, :revenue, :salary_expectation, :sex, :skill, :zip_code, :month, :day, :sexy, :avatar, :choose, :profiles, :profiles_attributes
+  attr_accessible :accounts, :certificates, :choose_file, :contact_address, :country, :date_birth, :email, :excepetation_industry, :excepetation_job, :experience_year, :job_status, :landline, :language_ability, :mobile, :name, :number, :pinyin, :revenue, :salary_expectation, :sex, :skill, :zip_code, :month, :day,  :avatar, :choose, :profiles, :profiles_attributes
   #attr_accessible :avatar
     has_attached_file :avatar, :styles => {  :thumb => "110x110>" }, :default_url => "/assets/rails.png"
     has_attached_file :choose, :styles => {  :thumb => "110x110>" }, :default_url => "/assets/rails.png"
   has_many :profiles
-  has_many :choose_files
+  #has_many :choose_files
   accepts_nested_attributes_for :profiles
 end
